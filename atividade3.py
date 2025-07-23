a3a841c9dbf86732af8ae53d096a4a21cb0edaf2
Maria Beatriz <mariabeatrixyz@gmail.com>
2025-07-22 23:49:32 -0300

    atividade 3 feita

---
+++ atividade3.py
@@ -0,0 +1,12 @@
+#Calculadora de Volume
+
+#Dimensões da caixa(cm)
+Comprimento = 12
+Largura = 14
+Altura = 20
+
+#Cálculo do Volume
+volume = Comprimento*Largura*Altura
+
+#Exibição do resultado
+print("O volume da caixa é:", volume, "cm³")
